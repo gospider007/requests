dbdc359600be04435da6ab1fdb84e8a67e981f82
bxd <2216403312@qq.com>
2023-09-20 11:39:56 +0800

    优化代码

--- headers.go
+++ headers.go
@@ -9,7 +9,7 @@ import (
 	"github.com/tidwall/gjson"
 )
 
-var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.188"
+var UserAgent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36 Edg/117.0.2045.31"
 var AcceptLanguage = "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
 
 // 请求操作========================================================================= start
