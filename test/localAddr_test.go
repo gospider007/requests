abadadeb8123afcdb91f68037f830ec70702f5d2
bxd <2216403312@qq.com>
2023-11-20 22:00:08 +0800

    add localAddr test

---
+++ test/localAddr_test.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"net"
+	"testing"
+
+	"github.com/gospider007/requests"
+)
+
+func TestLocalAddr(t *testing.T) {
+	resp, err := requests.Get(nil, "https://httpbin.org/anything", requests.RequestOption{
+		LocalAddr: &net.TCPAddr{ //set dns server
+			IP: net.ParseIP("192.168.1.239"),
+		},
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.StatusCode() != 200 {
+		t.Fatal("http status code is not 200")
+	}
+}
