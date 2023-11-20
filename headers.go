d5410e4ab1e9d4297ed66ac1f9876dabd35f47b6
bxd <2216403312@qq.com>
2023-11-20 15:38:13 +0800

    update headers version

--- headers.go
+++ headers.go
@@ -5,10 +5,10 @@ import (
 )
 
 const (
-	chromeV        = "117"
-	edgeV          = "117"
-	UserAgent      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + chromeV + ".0.0.0 Safari/537.36 Edg/" + edgeV + ".0.2045.31"
-	SecChUa        = `"Chromium";v="%s` + chromeV + `", "Microsoft Edge";v="` + edgeV + `", "Not=A?Brand";v="99"`
+	chromeV        = "120"
+	chromeE        = ".0.6099.28"
+	UserAgent      = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/" + chromeV + ".0.0.0 Safari/537.36 Edg/" + chromeV + chromeE
+	SecChUa        = `"Chromium";v="%s` + chromeV + `", "Microsoft Edge";v="` + chromeV + `", "Not=A?Brand";v="99"`
 	AcceptLanguage = "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6"
 )
 
