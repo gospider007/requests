c1438ce9a24d7122f5ad0090ce398dd51451c916
gospider <2216403312@qq.com>
2024-03-16 11:53:31 +0800

    sync

--- test/fingerprint/http2_test.go
+++ test/fingerprint/http2_test.go
@@ -10,7 +10,7 @@ import (
 
 func TestH2(t *testing.T) {
 	j := "1:65536,2:0,4:6291456,6:262144|15663105|0|m,a,s,p"
-	j2 := "1:65536;2:0;4:6291456;6:262144|15663105|1:true:0:255|a,m,p,s"
+	j2 := "1:65536;2:0;4:6291456;6:262144|15663105|0|m,a,s,p"
 	h2Spec, err := ja3.CreateH2SpecWithStr(j) //create h2 spec with string
 	if err != nil {
 		t.Fatal(err)
@@ -19,6 +19,7 @@ func TestH2(t *testing.T) {
 	resp, err := requests.Get(nil, "https://tools.scrapfly.io/api/fp/anything", requests.RequestOption{
 		H2Ja3Spec: h2Spec, //set h2 spec
 	})
+	// log.Print(resp.Text())
 	if err != nil {
 		t.Fatal(err)
 	}
