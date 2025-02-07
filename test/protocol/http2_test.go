aa74e9fdca5374060210105d70ac539aaf9dc65b
gospider <2216403312@qq.com>
2025-02-07 11:10:11 +0800

    sync

--- test/protocol/http2_test.go
+++ test/protocol/http2_test.go
@@ -18,15 +18,17 @@ func TestHttp2(t *testing.T) {
 	if resp.Proto() != "HTTP/2.0" {
 		t.Error("resp.Proto!= HTTP/2.0")
 	}
-	resp, err = requests.Get(context.TODO(), "https://mp.weixin.qq.com")
-	if err != nil {
-		t.Error(err)
-	}
-	if resp.StatusCode() != 200 {
-		t.Error("resp.StatusCode!= 200")
-	}
-	if resp.Proto() != "HTTP/2.0" {
-		t.Error("resp.Proto!= HTTP/2.0")
+	for range 3 {
+		resp, err = requests.Get(context.TODO(), "https://mp.weixin.qq.com")
+		if err != nil {
+			t.Error(err)
+		}
+		if resp.StatusCode() != 200 {
+			t.Error("resp.StatusCode!= 200")
+		}
+		if resp.Proto() != "HTTP/2.0" {
+			t.Error("resp.Proto!= HTTP/2.0")
+		}
 	}
 	resp, err = requests.Post(context.TODO(), "https://mp.weixin.qq.com", requests.RequestOption{Body: "fasfasfsdfdssdsfasdfasdfsadfsdf对方是大翻身大翻身大翻身对方的身份"})
 	if err != nil {
